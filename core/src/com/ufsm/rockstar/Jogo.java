013a8e9029f7f76fd96144ec1f1e5c5be40fefd2
NatasFX <nataschmitt02@gmail.com>
2021-08-21 13:15:57 -0300

    Started game screen

---
+++ core/src/com/ufsm/rockstar/Jogo.java
@@ -0,0 +1,176 @@
+package com.ufsm.rockstar;
+
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.BitmapFontCache;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class Jogo implements Screen, InputProcessor {
+
+    UfsmRockstar jogo;
+    //screen stuff
+    private Camera camera;      //the camera to gdx make their projections
+    private Viewport viewport;  //our window to display the game
+    private SpriteBatch batch;  //sprite batches
+
+    Color c; //Background color
+
+    //música
+    Music music;
+
+    //animação
+    Animation<TextureRegion> animacao;
+
+    Array<Texture> pads;
+    Texture pad3;
+    Texture pad5;
+
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+
+    private boolean pressed(int keycode) {
+        return Gdx.input.isKeyPressed(keycode);
+    }
+
+    public Jogo(UfsmRockstar jogo) {
+        this.jogo = jogo;
+
+        camera = new OrthographicCamera();
+        viewport = new StretchViewport(WIDTH, HEIGHT, camera);
+
+        batch = new SpriteBatch();
+
+        c = new Color();
+
+        animacao = utils.loadDancarino();
+
+        pads = new Array<Texture>();
+
+        for (int i = 1; i <= 5; i++)
+            pads.add(new Texture(Gdx.files.internal("imagens/" + i + ".png")));
+
+        pad3 = new Texture(Gdx.files.internal("imagens/middle_3.png"));
+        pad5 = new Texture(Gdx.files.internal("imagens/outs_5.png"));
+
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(this);
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(c);
+
+        batch.begin();
+        batch.draw(pad5, 100, 25, 600, 340);
+        batch.draw(pad3, 100, 25, 600, 340);
+
+
+        for (int i = 0; i < 5; i++) {
+            if (pressed(Input.Keys.H)) {
+                batch.draw(pads.get(0), 100, 25, 600, 340);
+            }
+            if (pressed(Input.Keys.J)) {
+                batch.draw(pads.get(1), 100, 25, 600, 340);
+            }
+            if (pressed(Input.Keys.K)) {
+                batch.draw(pads.get(2), 100, 25, 600, 340);
+            }
+            if (pressed(Input.Keys.L)) {
+                batch.draw(pads.get(3), 100, 25, 600, 340);
+            }
+//            if (pressed(Input.Keys.Cedilla)) {
+//                batch.draw(pads.get(1), 100, 25, 600, 340);
+//            }
+
+        }
+        batch.end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        System.out.println(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        System.out.println(Character.getNumericValue(character));
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
+}
--- core/src/com/ufsm/rockstar/UfsmRockstar.java
+++ core/src/com/ufsm/rockstar/UfsmRockstar.java
@@ -1,9 +1,5 @@
 package com.ufsm.rockstar;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.Game;
 
 import com.ufsm.rockstar.GameScreen;
